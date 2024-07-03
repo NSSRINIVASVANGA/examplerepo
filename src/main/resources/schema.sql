9235d6f110a1aebd9e5af60736566d458a94d842
Vanga Nataraja Shanmukha Srinivas <156287911+NSSRINIVASVANGA@users.noreply.github.com>
2024-07-03 11:56:11 +0530

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -51,17 +51,6 @@ CREATE TABLE if not exists orders (
     status VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE if not exists order_item (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    order_id BIGINT NOT NULL,
-    item_id BIGINT NOT NULL,
-    quantity INT NOT NULL,
-    restock_order_id BIGINT,
-    FOREIGN KEY (order_id) REFERENCES orders(id),
-    FOREIGN KEY (item_id) REFERENCES item(id),
-    FOREIGN KEY (restock_order_id) REFERENCES restock_order(id)
-);
-
 CREATE TABLE if not exists supplier (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -74,4 +63,15 @@ CREATE TABLE if not exists restock_order (
     order_date DATE NOT NULL,
     status VARCHAR(255) NOT NULL,
     FOREIGN KEY (supplier_id) REFERENCES supplier(id)
+);
+
+CREATE TABLE if not exists order_item (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    order_id BIGINT NOT NULL,
+    item_id BIGINT NOT NULL,
+    quantity INT NOT NULL,
+    restock_order_id BIGINT,
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (item_id) REFERENCES item(id),
+    FOREIGN KEY (restock_order_id) REFERENCES restock_order(id)
 );
